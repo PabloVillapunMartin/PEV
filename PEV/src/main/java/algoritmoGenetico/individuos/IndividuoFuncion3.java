5f5a5b519710bc6d3fc43e8e3285acbea1af8ba9
PabloVillapunMartin <48910465+PabloVillapunMartin@users.noreply.github.com>
2022-03-06 11:56:02 +0100

    IndividuoFuncion3 done

--- PEV/src/main/java/algoritmoGenetico/individuos/IndividuoFuncion3.java
+++ PEV/src/main/java/algoritmoGenetico/individuos/IndividuoFuncion3.java
@@ -1,38 +1,82 @@
 package algoritmoGenetico.individuos;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class IndividuoFuncion3 extends Individuo<Boolean>{
 
+	private double min[];
+	private double max[];
+	private double valorError;
+	
+	private Random rand;
+	
 	public IndividuoFuncion3() {
+		inicializaValores();
 		
+		for(int i = 0; i < this.cromosoma.length; i++)  
+			this.cromosoma[i] = this.rand.nextBoolean();
 	}
 
 	@Override
 	public double getValor() {
-		// TODO Auto-generated method stub
-		return 0;
+		double x1 = this.getFenotipo(0);
+		double x2 = this.getFenotipo(1);
+		double parte1 = -(x2 + 47) * Math.sin(Math.sqrt(Math.abs(x2 + (x1/2) + 47)));
+		double parte2 = -x1 * Math.sin(Math.sqrt(Math.abs(x1 - (x2 + 47))));
+		
+		return parte1 + parte2;
 	}
 
 	@Override
 	public double getFitness() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.getValor() + + 959.6407;
 	}
 
 	@Override
 	public void mutar(double probMutacion, Random r) {
-		// TODO Auto-generated method stub
-		
+		for (int i=0; i < this.cromosoma.length; i++) {
+			if (r.nextDouble() < probMutacion) 
+				cromosoma[i] = r.nextBoolean();		
+		}
 	}
 
 	protected void inicializaValores() {
-		// TODO Auto-generated method stub
+		this.valorError = 0.001;
+		this.min = new double[] {-512, -512};
+		this.max = new double[] {512, 512};
+		this.tamGenes = new int[2];
+		this.tamGenes[0] = this.tamGen(this.valorError, min[0], max[0]);
+		this.tamGenes[1] = this.tamGen(this.valorError, min[1], max[1]);
+		this.cromosoma = new Boolean[tamGenes[0] + tamGenes[1]];
 		
+		rand = new Random();
 	}
 
 	public int compareTo(Individuo o) {
-		// TODO Auto-generated method stub
-		return 0;
+		if(this.getValor() - o.getValor() > 0)
+			return 1;
+		if(this.getValor() - o.getValor() < 0)
+			return -1;
+		else return 0;
 	}
+	
+	private double getFenotipo(int genIndex) {
+		return this.min[genIndex] + bin2dec(genIndex) *
+				((this.max[genIndex] - this.min[genIndex]) / (Math.pow(2, this.tamGenes[genIndex]) - 1.0));
+	}
+	
+	private double bin2dec(int genIndex) {
+		int inicio = 0;
+		if(genIndex == 1)
+			inicio += this.tamGenes[0];
+		Boolean values[] = Arrays.copyOfRange(this.cromosoma, inicio, inicio + this.tamGenes[genIndex]);
+		
+		long result = 0;
+        for (boolean bit : values) {
+            result = result * 2 + (bit ? 1 : 0);
+        }
+
+        return result;
+    }
 }
