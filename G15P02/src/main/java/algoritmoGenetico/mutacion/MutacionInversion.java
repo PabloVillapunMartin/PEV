395ca4f3f2c16b27c92b628ed1e32e50d3c2e7ec
PabloVillapunMartin <48910465+PabloVillapunMartin@users.noreply.github.com>
2022-03-28 20:18:34 +0200

    MutacionInversion

---
+++ G15P02/src/main/java/algoritmoGenetico/mutacion/MutacionInversion.java
@@ -0,0 +1,41 @@
+package algoritmoGenetico.mutacion;
+
+import java.util.Arrays;
+
+import algoritmoGenetico.AlgoritmoGenetico.FuncionIndividuo;
+import algoritmoGenetico.individuos.Individuo;
+
+public class MutacionInversion extends Mutacion {
+
+	public MutacionInversion(double probMutacion, FuncionIndividuo funcion) {
+		super(probMutacion, funcion);
+	}
+
+	@Override
+	public Individuo[] mutar(Individuo[] individuos) {
+		int n  = individuos.length;
+
+		for(int i = 0; i < n; i++){
+			if(this.rnd.nextFloat() < this.probMutacion)
+				mutarIndividuo(individuos[i]);
+		}
+		return individuos;
+	}
+
+	private void mutarIndividuo(Individuo individuo) {
+		int corte1 = this.rnd.nextInt(individuo.getCromosoma().length);
+		int corte2 = this.rnd.nextInt(individuo.getCromosoma().length);
+		
+		if(corte1 > corte2) {
+			int aux = corte1;
+			corte1 = corte2;
+			corte2 = aux;
+		}
+		
+		Object[] tramo = Arrays.copyOfRange(individuo.getCromosoma(), corte1, corte2);
+		
+		for(int i = 0; i < tramo.length; ++i) {
+			individuo.getCromosoma()[corte1 + i] = tramo[tramo.length - i];
+		}
+	}
+}
